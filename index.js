fc6cc25491aac6ee6254703f7d16de1eea550398
Michal Radecki <odrzutowiec@gmail.com>
2018-06-20 21:39:53 +0200

    telnet option handling

--- index.js
+++ index.js
@@ -1,41 +1,82 @@
 const net = require('net')
 const stream = require('stream')
+const buffer = require('buffer').Buffer
 
-/*
-https://nodejs.org/api/stream.html#stream_implementing_a_transform_stream
-https://github.com/blinkdog/telnet-stream/blob/master/src/main/coffee/telnetInput.coffee
-http://tintin.sourceforge.net/protocols/mtts/
-https://cs.stanford.edu/~miles/iso8859.html
-https://en.wikipedia.org/wiki/Telnet
-https://tools.ietf.org/html/rfc854
-*/
+const COMMAND = Symbol()
+const OPTION = Symbol()
+const SUBNEG = Symbol()
+const SBNGCMD = Symbol()
 
-const SE = 240 // End of subnegotiation parameters
-const NOP = 241 // No operation
-const DATA = 242 // The data stream portion of a Synch
-const BRK = 243 // NVT character Break
-const IP = 244 // The function Interrupt_Process
-const AO = 245 // The function Abort_output
-const AYT = 246 // The function Are_You_There
-const EC = 247 // The function Erase_character
-const EL = 248 // The function Erase_Line
-const GA = 249 // The Go_ahead  signal
-const SB = 250 // Indicates that what follows is
-const WILL = 251 // Indicates the desire to begin
-const WONT = 252 // Indicates the refusal to perform
-const DO = 253 // From now on expecting
-const DONT = 254 // No longer expecting
-const IAC = 255 // Data Byte 255 (Interpret As Command)
+const SUBEND = 240
+const SUBBEG = 250
+const WILL = 251
+const WONT = 252
+const DO = 253
+const DONT = 254
+const IAC = 255
 
 class Output extends stream.Transform {
   constructor() {
     super()
     this.setEncoding('latin1')
+    this.state = undefined
+    this.command = undefined
   }
   _transform(chunk, encoding, callback) {
-    console.log(encoding)
-    this.push(chunk)
+    const newchunk = buffer.alloc(chunk.length)
+    let index = 0
+    for (let byte of chunk) {
+      const newbyte = this.handle(byte)
+      if (newbyte !== undefined) {
+        newchunk[index++] = newbyte
+      }
+    }
+    this.push(newchunk)
     callback()
+  }
+  handle(byte) {
+    switch (this.state) {
+      case COMMAND:
+        switch (byte) {
+          case WILL:
+          case WONT:
+          case DO:
+          case DONT:
+            this.state = OPTION
+            this.command = byte
+            return undefined
+
+          default:
+            this.emit('command', { command: byte })
+            return undefined
+        }
+
+      case OPTION:
+        this.emit('command', { command: this.command, option: byte })
+        this.state = undefined
+        this.command = undefined
+        return undefined
+
+      case SUBNEG:
+        this.state = undefined
+        return byte
+
+      case SBNGCMD:
+        this.state = undefined
+        return byte
+
+      case undefined:
+      default:
+        switch (byte) {
+          case IAC:
+            this.state = COMMAND
+            return undefined
+
+          default:
+            return byte
+        }
+
+    }
   }
 }
 
@@ -49,15 +90,10 @@ class Socket extends net.Socket {
 
 const socket = new Socket()
 socket.connect(23, 'aardwolf.org')
-socket.out.on('data', args => {
-  for (char of args) {
-    const code = char.charCodeAt() 
-    if (code > 127) {
-      console.log('=>', code)
-    }
-    else {
-      process.stdout.write(char)
-    }
-  }
+socket.out.on('data', data => {
+  process.stdout.write(data)
+})
+socket.out.on('command', command => {
+  console.log('command', command)
 })
 
